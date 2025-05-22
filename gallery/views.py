50a7926c2ac176d33c455c722c008e3c2471bcb0
imranprof <imranpers@gmail.com>
2025-05-22 23:40:34 +0600

    feat: add img form and view controller

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Image
+from .forms import ImageForm
 
-# Create your views here.
+
+def image_upload(request):
+  if request.method == 'POST':
+    form = ImageForm(request.POST, request.FILES)
+    if form.is_valid():
+      form.save()
+      return redirect('image_list')
+    else:
+      form = ImageForm()
+    return render(request, 'gallery/upload.html', {'form': form})
+
+def image_list(request):
+  images = Image.objects.all()
+  return render(request, 'gallery/list.html', {'images': images})
